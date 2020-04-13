ce097742c53707bcb9fc023aab86df36d7cda4dc
cahyo-joyo <61173367+cahyo-joyo@users.noreply.github.com>
2020-04-13 21:40:39 +0700

    Add files via upload

--- praktikum10/formvalidation.php
+++ praktikum10/formvalidation.php
@@ -28,7 +28,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	else 
 	{
 		$nama = cek_input($_POST["nama"]);
-		if (!preg_match("/^[a-zA-A ]*$/", $nama)) 
+		if (!preg_match("/^[a-zA-Z ]*$/", $nama)) 
 		{
 			$error_nama = "Inputan hanya boleh huruf dan spasi";
 		}
